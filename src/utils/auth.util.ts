af50ea83b5f9faedbf4033f3f9c0e5ee94a4c564
NTGiang <giangcoivipk50@gmail.com>
2023-09-11 21:11:08 +0700

    update

--- src/utils/auth.util.ts
+++ src/utils/auth.util.ts
@@ -9,9 +9,9 @@ export const clearLS = () => {
 
 export const getProfileFromLS = () => {
   const result = localStorage.getItem('profile')
-  return result ? JSON.parse(result) : null
+  return result ? (JSON.parse(result) as { username: string; password: string; id: number }) : null
 }
 
-export const setProfileToLS = (profile: { username: string; password: string }) => {
+export const setProfileToLS = (profile: { username: string; password: string; id: number }) => {
   localStorage.setItem('profile', JSON.stringify(profile))
 }
