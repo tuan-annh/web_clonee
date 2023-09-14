cff7227a2824b6037094ba0554de098ebc336be2
tuan-annh <tuananh4102001@gmail.com>
2023-09-14 23:47:01 +0700

    update

--- src/pages/Register/Register.tsx
+++ src/pages/Register/Register.tsx
@@ -208,7 +208,7 @@ function Register() {
             {debounce && (
               <svg
                 aria-hidden='true'
-                className='mr-2 h-4 w-4 animate-spin fill-blue-600 text-gray-200 dark:text-gray-600'
+                className='mr-2 h-5 w-5 animate-spin fill-blue-600 text-gray-200 dark:text-gray-600'
                 viewBox='0 0 100 101'
                 fill='none'
                 xmlns='http://www.w3.org/2000/svg'
